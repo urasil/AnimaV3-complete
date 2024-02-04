61aef28a351d4ee339622e340861d795e592d6dd
Connor Zhan <yaalyy@outlook.com>
2024-02-04 01:24:47 +0000

    resolved issue 'voice banking directly moves to next page but not wait for the backend response

---
+++ frontend/Core/ButtonHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace dotnetAnima.Core
+{
+    public static class ButtonHelper
+    {
+        public static void DisableButton(Button buttonName, bool status) 
+        {
+            if(!status)
+            {
+                buttonName.Opacity = 0.3;
+                buttonName.IsEnabled = false;
+            }
+            else
+            {
+                buttonName.Opacity = 1;
+                buttonName.IsEnabled = true;
+            }
+        }
+    }
+}
