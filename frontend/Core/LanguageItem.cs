040440a0ff7440e60a94687925bb6219ed7a72e4
Connor Zhan <yaalyy@outlook.com>
2024-03-05 02:23:23 +0000

    added French and Portuguese support in frontend

---
+++ frontend/Core/LanguageItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetAnima.Core
+{
+    // the item in the language dropdown list, {country_icon + text}
+    public class LanguageItem  
+    {
+        public string icon { get; set; }
+        public string text {  get; set; }
+
+        public LanguageItem(string icon, string text) { 
+            this.icon = icon;
+            this.text = text;
+        }
+    }
+}
