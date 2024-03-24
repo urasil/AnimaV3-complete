2c7d3b4ee577842996323431be575c13ad50ae42
urasil <zcabuas@ucl.ac.uk>
2024-03-24 23:47:36 +0000

    fix backend not starting

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,22 +17,23 @@ public partial class MainWindow : Window {
     }
 
     static void StartExecutable() {
-        try {
-            Console.WriteLine(Directory.GetCurrentDirectory());
-            string executablePath = "backend\\ANIMA\\anima.exe";
+    try {
+        Console.WriteLine(Directory.GetCurrentDirectory());
+            string executablePath = Path.Combine(Directory.GetCurrentDirectory(), "backend", "ANIMA", "anima.exe");
 
-            ProcessStartInfo startInfo = new ProcessStartInfo(executablePath);
-            startInfo.WindowStyle =
-                ProcessWindowStyle.Hidden;  // hide the backend terminal window
-            startInfo.CreateNoWindow = true;
-            startInfo.WorkingDirectory = "backend\\ANIMA\\";
+            ProcessStartInfo startInfo = new ProcessStartInfo(executablePath)
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,  // hide the backend terminal window
+                CreateNoWindow = true,
+                WorkingDirectory = "backend\\ANIMA\\"
+            };
 
             executableProcess = Process.Start(startInfo);
-            Console.WriteLine("Executable started successfully.");
-        } catch (Exception ex) {
-            Console.WriteLine($"Error starting executable: {ex.Message}");
-        }
+        Console.WriteLine("Executable started successfully.");
+    } catch (Exception ex) {
+        Console.WriteLine($"Error starting executable: {ex.Message}");
     }
+}
 
     static void CloseWindow(object sender,
                             System.ComponentModel.CancelEventArgs e) {
